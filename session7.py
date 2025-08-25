9122e6b8e7979cd6be08e51f84dc428c65a0255d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-25 17:55:31 +0330

    Add files via upload

--- session7.py
+++ session7.py
@@ -20,13 +20,57 @@
 
 
 
-names = []
+# names = []
 
-while True:
-    n = input("enter a name: ")
-    if n[0] == "a" or n[0] ==  "A":
-        names.append(n)
-    if input("Do you want to quit (y or n): ") == "y":
-        break
+# while True:
+#     n = input("enter a name: ")
+#     if n[0] == "a" or n[0] ==  "A":
+#         names.append(n)
+#     if input("Do you want to quit (y or n): ") == "y":
+#         break
 
-print(names)
+# print(names)
+
+
+
+
+
+# even_numbers = []
+# odd_numbers = []
+
+# for i in range(5):
+#     n = int(input("enter a number: "))
+#     if n % 2 == 0:
+#         even_numbers.append(n)
+#     else:
+#         odd_numbers.append(n)
+
+# print("even numbers:", even_numbers)
+# print("odd numbers:", odd_numbers)
+
+# print("sum of evens:", sum(even_numbers))
+# print("sum of odds:", sum(odd_numbers))
+
+
+import turtle
+turtle.register_shape("apple.gif")
+turtle.shape("apple.gif") # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+turtle.color("red")
+turtle.shapesize(2)
+turtle.pensize(2)
+
+sides = int(turtle.textinput("sides", "how many sides you want?"))
+
+turtle.begin_fill()
+for i in range(sides):
+    turtle.forward(100)
+    turtle.left(360/sides)
+turtle.end_fill()
+turtle.penup()
+turtle.goto(-300, 200)
+turtle.stamp()
+turtle.goto(300, 200)
+turtle.stamp()
+turtle.goto(300, -200)
+turtle.stamp()
+turtle.done()
