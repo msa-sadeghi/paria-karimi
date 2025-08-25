4db8d74d655af05e324b169e9c342971710ac837
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-25 17:21:49 +0330

    s7

--- session7.py
+++ session7.py
@@ -11,9 +11,22 @@
 # print(names)
 
 
-numbers = []
-for i in range(3):
-    new_number =  int(input("enter a number: "))
-    numbers.append(new_number)
-print(numbers)
-print(sum(numbers))
+# numbers = []
+# for i in range(3):
+#     new_number =  int(input("enter a number: "))
+#     numbers.append(new_number)
+# print(numbers)
+# print(sum(numbers))
+
+
+
+names = []
+
+while True:
+    n = input("enter a name: ")
+    if n[0] == "a" or n[0] ==  "A":
+        names.append(n)
+    if input("Do you want to quit (y or n): ") == "y":
+        break
+
+print(names)
