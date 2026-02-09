0f64fba30fddb6cf96d6fd135bf0bae555494145
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-09 17:57:36 +0330

    codes

--- main.py
+++ main.py
@@ -5,50 +5,43 @@ from obstacle import Obstacle
 WIDTH = 1000
 HEIGHT = 640
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 60
-CLOCK = pygame.time.Clock()
 
 cat = Player('Cat', 300, 200)
 crate_image = pygame.image.load("Object/Crate.png")
 stone_image = pygame.image.load("Object/Stone.png")
 obstacle_group = pygame.sprite.Group()
+
+crate_width = crate_image.get_width()
+stone_width = stone_image.get_width()
 crate1 = Obstacle('crate',crate_image, 100, HEIGHT, obstacle_group)
-crate2 = Obstacle('crate',crate_image, 100 + crate_image.get_size()[0], HEIGHT, obstacle_group)
-crate2 = Obstacle('crate',crate_image, 100 + 2 *crate_image.get_size()[0], HEIGHT, obstacle_group)
-crate3 = Obstacle('crate',crate_image, 100 + 2 *crate_image.get_size()[0], HEIGHT, obstacle_group)
-# crate3 = Obstacle('crate',crate_image, 100 + 2 *crate_image.get_size()[0], HEIGHT - crate_image.get_size()[1], obstacle_group)
+crate2 = Obstacle('crate',crate_image, 100 + crate_width, HEIGHT, obstacle_group)
+crate2 = Obstacle('crate',crate_image, 100 + 2 * crate_width, HEIGHT, obstacle_group)
 stone1 = Obstacle('stone',stone_image, 700, HEIGHT, obstacle_group)
-stone2 = Obstacle('stone',stone_image, 700 + stone_image.get_size()[0] , HEIGHT, obstacle_group)
+stone2 = Obstacle('stone',stone_image, 700 + stone_width , HEIGHT, obstacle_group)
 
 
 scroll = 0
-scroll_count = 1
 FPS = 60
 clock = pygame.time.Clock()
-
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running  = False
     screen.fill("lightgreen")  
-    CLOCK.tick(FPS)
-
-    if cat.rect.right >= scroll_count * WIDTH:
-        scroll = -100
-        scroll_count += .2
-    
-
-    elif cat.rect.left < (scroll_count - 1) * WIDTH and  cat.rect.left > 0:
-        scroll_count -= .2
-        scroll = 100
+    cat.move(obstacle_group)
+    if cat.rect.right > WIDTH - 200:
+        scroll = -5
+        cat.rect.right = WIDTH - 200
+    elif cat.rect.left < 200:
+        cat.rect.left = 200
+        scroll = 4
     else:
         scroll = 0
-
     cat.draw(screen)
-    cat.move(obstacle_group)
-    obstacle_group.draw(screen)
     obstacle_group.update(scroll)
+    obstacle_group.draw(screen)
+
     pygame.display.update()
     clock.tick(FPS)
 
